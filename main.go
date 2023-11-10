f462b7384d6ad983f5cc43ae90326052987762c0
AEsri-Dream <149231217+AEsri-Dream@users.noreply.github.com>
2023-11-10 22:31:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -36,9 +36,9 @@ import (
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/sleepmanage" // 统计睡眠时间
 
-	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/atri" // ATRI词库
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/atri" // ATRI词库
 
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/al1s" // AL1S词库
+	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/al1s" // AL1S词库
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/manager" // 群管
 
