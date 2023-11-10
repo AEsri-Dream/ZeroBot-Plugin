a8a5b37abee3f5dc21dc7a71117256d40e7f98f8
AEsri-Dream <149231217+AEsri-Dream@users.noreply.github.com>
2023-11-10 21:50:28 +0800

    Update al1s.go

--- plugin/AL-1S/al1s.go
+++ plugin/AL-1S/al1s.go
@@ -1,9 +1,8 @@
 /*
 Package atri 本文件基于 https://github.com/Kyomotoi/ATRI
-为 Golang 移植版，语料、素材均来自上述项目
 本项目遵守 AGPL v3 协议进行开源
 */
-package atri
+package al1s
 
 import (
 	"encoding/base64"
@@ -39,25 +38,18 @@ func randText(text ...string) message.MessageSegment {
 	return message.Text(text[rand.Intn(len(text))])
 }
 
-// isAtriSleeping 凌晨0点到6点，ATRI 在睡觉，不回应任何请求
-func isAtriSleeping(*zero.Ctx) bool {
-	if now := time.Now().Hour(); now >= 1 && now < 6 {
-		return false
-	}
-	return true
-}
 
 func init() { // 插件主体
 	engine := control.AutoRegister(&ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
-		Brief:            "atri人格文本回复",
-		Help: "本插件基于 ATRI ，为 Golang 移植版\n" +
-			"- ATRI醒醒\n- ATRI睡吧\n- 萝卜子\n- 喜欢 | 爱你 | 爱 | suki | daisuki | すき | 好き | 贴贴 | 老婆 | 亲一个 | mua\n" +
+		Brief:            "AL-1S人格文本回复",
+		Help: "本插件基于 ATRI ，梦的魔改版\n" +
+			"- \n- 喜欢 | 爱你 | 爱 | suki | daisuki | すき | 好き | 贴贴 | 老婆 | 亲一个 | mua\n" +
 			"- 草你妈 | 操你妈 | 脑瘫 | 废柴 | fw | 废物 | 战斗 | 爬 | 爪巴 | sb | SB | 傻B\n- 早安 | 早哇 | 早上好 | ohayo | 哦哈哟 | お早う | 早好 | 早 | 早早早\n" +
 			"- 中午好 | 午安 | 午好\n- 晚安 | oyasuminasai | おやすみなさい | 晚好 | 晚上好\n- 高性能 | 太棒了 | すごい | sugoi | 斯国一 | よかった\n" +
 			"- 没事 | 没关系 | 大丈夫 | 还好 | 不要紧 | 没出大问题 | 没伤到哪\n- 好吗 | 是吗 | 行不行 | 能不能 | 可不可以\n- 啊这\n- 我好了\n- ？ | ? | ¿\n" +
 			"- 离谱\n- 答应我",
-		PublicDataFolder: "Atri",
+		PublicDataFolder: "AL-1S",
 		OnEnable: func(ctx *zero.Ctx) {
 			ctx.SendChain(message.Text("嗯呜呜……sensei……？"))
 		},
@@ -76,13 +68,29 @@ func init() { // 插件主体
 				ctx.SendChain(dgtr.randRecord("RocketPunch.amr"))
 			}
 		})
-	engine.OnFullMatchGroup([]string{"喜欢", "爱你", "爱", "suki", "daisuki", "すき", "好き", "贴贴", "老婆", "亲一个", "mua"}, zero.OnlyToMe).SetBlock(true).
+	engine.OnFullMatchGroup([]string{"喜欢", "爱你", "爱", "suki", "daisuki", "すき", "好き"}, zero.OnlyToMe).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			ctx.SendChain(dgtr.randImage("SUKI.jpg", "SUKI1.jpg", "SUKI2.png"))
+			ctx.SendChain(dgtr.randImage("X1.png", "X2.png", "X3.png"))
 		})
-	engine.OnKeywordGroup([]string{"草你妈", "操你妈", "脑瘫", "废柴", "fw", "five", "废物", "战斗", "爬", "爪巴", "sb", "SB", "傻B"}, zero.OnlyToMe).SetBlock(true).
+	engine.OnFullMatchGroup([]string{"讨厌", "讨厌你", "不爱了", "没爱了", "不喜欢你了", "不喜欢", "一边去", "谁问你了", "这机器人好烦", "你好烦"}, zero.OnlyToMe).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			ctx.SendChain(dgtr.randImage("FN.jpg", "WQ.jpg", "WQ1.jpg"))
+			ctx.SendChain(dgtr.randImage("K1.png", "K2.png", "K3.png", "K4.png"))
+		})
+	engine.OnFullMatchGroup([]string{"透透", "透透老婆", "开银趴", "炼铜", "萝莉控", "变态", "Hentai", "hentai"}, zero.OnlyToMe).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			ctx.SendChain(dgtr.randImage("BY1.png", "BY2.png", "HY1.png", "HY2.png", "HY3.png"))
+		})
+	engine.OnFullMatchGroup([]string{"贴贴", "老婆", "我爱你", "摸摸", "亲一个", "mua"}, zero.OnlyToMe).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			ctx.SendChain(dgtr.randImage("X3.png"))
+		})
+	engine.OnFullMatchGroup([]string{"骂我", "狠狠骂我", "tister!", "tister！", "Tister!", "Tister！"}, zero.OnlyToMe).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			ctx.SendChain(dgtr.randImage("HY2.png", "HY3.png"))
+		})
+	engine.OnKeywordGroup([]string{"啊米诺斯", "草你妈", "曹尼玛", "神经病", "操你妈", "脑瘫", "废柴", "fw", "kkp", "five", "废物", "战斗", "爬", "爪巴", "sb", "SB", "滚", "傻B"}, zero.OnlyToMe).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			ctx.SendChain(dgtr.randImage("KB1.png", "KB2.png", "KB3.png", "N1.png", "N2.png", "N3.png"))
 		})
 	engine.OnFullMatchGroup([]string{"早安", "早哇", "早上好", "ohayo", "哦哈哟", "お早う", "早好", "早", "早早早"}).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
@@ -92,9 +100,9 @@ func init() { // 插件主体
 				ctx.SendChain(message.Reply(ctx.Event.MessageID), randText(
 					"zzzz......",
 					"zzzzzzzz......",
-					"zzz...好涩哦..zzz....",
-					"别...不要..zzz..那..zzz..",
-					"嘻嘻..zzz..呐~..zzzz..",
+					"zzz...sensei..zzz....",
+					"sensei...不要..zzz..那..zzz..",
+					"sensei..zzz..呐~..zzzz..",
 					"...zzz....哧溜哧溜....",
 				))
 			case now >= 6 && now < 9:
@@ -103,11 +111,11 @@ func init() { // 插件主体
 					"唔......吧唧...早上...哈啊啊~~~\n早上好......",
 					"早上好......",
 					"早上好呜......呼啊啊~~~~",
-					"啊......早上好。\n昨晚也很激情呢！",
-					"吧唧吧唧......怎么了...已经早上了么...",
+					"啊......早上好。\n昨晚也很激情呢！\n？爱丽丝是说游戏哦",
+					"senaei......怎么了...已经早上了么...",
 					"早上好！",
-					"......看起来像是傍晚，其实已经早上了吗？",
 					"早上好......欸~~~脸好近呢",
+					"邦邦卡邦！午安，sensei！爱丽丝今天精神饱满，准备好迎接新的冒险了！",
 				))
 			case now >= 9 && now < 18:
 				ctx.SendChain(message.Reply(ctx.Event.MessageID), randText(
@@ -150,17 +158,15 @@ func init() { // 插件主体
 				))
 			case now >= 6 && now < 11:
 				ctx.SendChain(message.Reply(ctx.Event.MessageID), randText(
-					"你可猝死算了吧！",
-					"？啊这",
-					"亲，这边建议赶快去睡觉呢~~~",
-					"不可忍不可忍不可忍！！为何这还不猝死！！",
+					"sensei可不要猝死！",
+					"？都这个时间了！",
+					"sensei，快点睡觉！",
 				))
 			case now >= 11 && now < 15:
 				ctx.SendChain(message.Reply(ctx.Event.MessageID), randText(
 					"午安w",
-					"午觉要好好睡哦，ATRI会陪伴在你身旁的w",
+					"午觉要好好睡哦，爱丽丝会陪伴在你身旁的w",
 					"嗯哼哼~睡吧，就像平常一样安眠吧~o(≧▽≦)o",
-					"睡你午觉去！哼唧！！",
 				))
 			case now >= 15 && now < 19:
 				ctx.SendChain(message.Reply(ctx.Event.MessageID), randText(
@@ -170,83 +176,41 @@ func init() { // 插件主体
 				))
 			case now >= 19 && now < 24:
 				ctx.SendChain(message.Reply(ctx.Event.MessageID), randText(
-					"嗯哼哼~睡吧，就像平常一样安眠吧~o(≧▽≦)o",
+					"嗯哼哼~睡吧，就像平常一样安眠吧，爱丽丝会一直陪在sensei身边的~o(≧▽≦)o",
 					"......(打瞌睡)",
-					"呼...呼...已经睡着了哦~...呼......",
-					"......我、我会在这守着你的，请务必好好睡着",
+					"呼...呼...爱丽丝已经睡着了哦~...呼......",
+					"......爱丽丝会在这守着你的",
 				))
 			}
 		})
-	engine.OnKeywordGroup([]string{"高性能", "太棒了", "すごい", "sugoi", "斯国一", "よかった"}, zero.OnlyToMe).SetBlock(true).
-		Handle(func(ctx *zero.Ctx) {
-			ctx.SendChain(randText(
-				"当然，我是高性能的嘛~！",
-				"小事一桩，我是高性能的嘛",
-				"怎么样？还是我比较高性能吧？",
-				"哼哼！我果然是高性能的呢！",
-				"因为我是高性能的嘛！嗯哼！",
-				"因为我是高性能的呢！",
-				"哎呀~，我可真是太高性能了",
-				"正是，因为我是高性能的",
-				"是的。我是高性能的嘛♪",
-				"毕竟我可是高性能的！",
-				"嘿嘿，我的高性能发挥出来啦♪",
-				"我果然是很高性能的机器人吧！",
-				"是吧！谁叫我这么高性能呢！哼哼！",
-				"交给我吧，有高性能的我陪着呢",
-				"呣......我的高性能，毫无遗憾地施展出来了......",
-			))
-		})
-	engine.OnKeywordGroup([]string{"没事", "没关系", "大丈夫", "还好", "不要紧", "没出大问题", "没伤到哪"}, zero.OnlyToMe).SetBlock(true).
-		Handle(func(ctx *zero.Ctx) {
-			ctx.SendChain(randText(
-				"当然，我是高性能的嘛~！",
-				"没事没事，因为我是高性能的嘛！嗯哼！",
-				"没事的，因为我是高性能的呢！",
-				"正是，因为我是高性能的",
-				"是的。我是高性能的嘛♪",
-				"毕竟我可是高性能的！",
-				"那种程度的事不算什么的。\n别看我这样，我可是高性能的",
-				"没问题的，我可是高性能的",
-			))
-		})
-
+	
 	engine.OnKeywordGroup([]string{"好吗", "是吗", "行不行", "能不能", "可不可以"}).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			if rand.Intn(2) == 0 {
-				ctx.SendChain(dgtr.randImage("YES.png", "NO.jpg"))
+				ctx.SendChain(dgtr.randImage("YES1.png", "YES2.png", "NO1.jpg", "NO2.jpg"))
 			}
 		})
-	engine.OnKeywordGroup([]string{"啊这"}).SetBlock(true).
+	engine.OnKeywordGroup([]string{"啊这", "AZ", "az"}).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			if rand.Intn(2) == 0 {
 				ctx.SendChain(dgtr.randImage("AZ.jpg", "AZ1.jpg"))
 			}
 		})
-	engine.OnKeywordGroup([]string{"我好了"}).SetBlock(true).
+	engine.OnKeywordGroup([]string{"我好了", "准备好了", "准备完成", "准备出发", "好了", "出发"}).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			ctx.SendChain(message.Reply(ctx.Event.MessageID), randText("不许好！", "憋回去！"))
+			ctx.SendChain(dgtr.randRecord("Aris_Formation_In_2.ogg", "Aris_Formation_Select.ogg", "Aris_Formation_In_1.ogg", "Formation_In_1.ogg", "Formation_In_2.ogg", "Formation_Select.ogg"))
 		})
-	engine.OnFullMatchGroup([]string{"？", "?", "¿"}).SetBlock(true).
+	engine.OnFullMatchGroup([]string{"？", "?", "¿", "离谱"}).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			switch rand.Intn(5) {
 			case 0:
 				ctx.SendChain(randText("?", "？", "嗯？", "(。´・ω・)ん?", "ん？"))
 			case 1, 2:
-				ctx.SendChain(dgtr.randImage("WH.jpg", "WH1.jpg", "WH2.jpg", "WH3.jpg"))
-			}
-		})
-	engine.OnKeyword("离谱").SetBlock(true).
-		Handle(func(ctx *zero.Ctx) {
-			switch rand.Intn(5) {
-			case 0:
-				ctx.SendChain(randText("?", "？", "嗯？", "(。´・ω・)ん?", "ん？"))
-			case 1, 2:
-				ctx.SendChain(dgtr.randImage("WH.jpg"))
+				ctx.SendChain(dgtr.randImage("WH1.jpg", "WH2.jpg"))
 			}
 		})
 	engine.OnKeyword("答应我", zero.OnlyToMe).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			ctx.SendChain(randText("我无法回应你的请求"))
+			ctx.SendChain(randText("爱丽丝会努力的！"))
 		})
 }
